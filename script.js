cb02693cc9d8d889bae5958731e37c5cec3b623c
michelg85 <miguelguyot85@gmail.com>
2022-09-14 14:36:15 -0300

    Cambios en JS

--- script.js
+++ script.js
@@ -1,59 +1,56 @@
 //Creo la clase de objeto producto como base para cargar los productos 
 class Producto {
-    constructor(nombre, id, precio, color, stock) {
+    constructor(nombre, id, precio) {
         this.nombre = nombre;
         this.id = id;
         this.precio = precio;
-        this.color = color;
-        this.stock = stock;
+       
     }
 }
 
 //Inicializo arrays para las categorias de los productos
-const remeras = [];
-const buzos = [];
-const pantalones = [];
+const tabla = [];
+const tonkatsu = [];
+const gyozas = [];
 
-//Cargo el stock de productos segun su categoria
-//REMERAS
-remeras.push(new Producto("Remera Cobra", 1, 2000, "Negro", 30));
-remeras.push(new Producto("Musculosa Good Times", 2, 1400, "Blanco", 10));
-remeras.push(new Producto("Remera Aeternum", 3, 1400, "Blanco", 20));
-remeras.push(new Producto("Remera Good Times", 4, 1400, "Blanco", 15));
-//BUZOS
-buzos.push(new Producto("Buzo Scream", 5, 4000, "Negro", 20));
-buzos.push(new Producto("Buzo Negro Oversized", 6, 3800, "Negro", 40));
-buzos.push(new Producto("Buzo Batik", 7, 3800, "Negro", 40));
-//PANTALONES
-pantalones.push(new Producto("Jogging NC", 8, 2900, "Negro", 10));
-pantalones.push(new Producto("Short Notte", 9, 1000, "Negro", 10));
+//Cargo los productos segun su categoria
+//TABLAS
+tabla.push(new Producto("Tabla Taiko", 1, 2000));
+tabla.push(new Producto("Tabla Taikito", 2, 1400));
+tabla.push(new Producto("Tabla simple", 3, 1200));
+tabla.push(new Producto("Tabla Veggie", 4, 1200));
+//TONKATSU
+tonkatsu.push(new Producto("Tonkatsu Cerdo", 5, 1200));
+//GYOZAS
+gyozas.push(new Producto("Gyozas Taiko", 8, 980));
+
 
 
 //Recorro los arrays para mostrarle los productos al usuario
-for (const remera of remeras) {
-    alert("ID (" + remera.id + ") - " + remera.nombre);
+for (const tabla of tabla) {
+    alert("ID (" + tabla.id + ") - " + tabla.nombre);
 }
-for (const buzo of buzos) {
-    alert("ID (" + buzo.id + ") - " + buzo.nombre);
+for (const tonkatsu of tonkatsu) {
+    alert("ID (" + tonkatsu.id + ") - " + tonkatsu.nombre);
 }
-for (const pantalon of pantalones) {
-    alert("ID (" + pantalon.id + ") - " + pantalon.nombre);
+for (const gyozas of gyozas) {
+    alert("ID (" + gyozas.id + ") - " + gyozas.nombre);
 }
 
-//Solicito al usuario el ID de la prenda
-let prendaSeleccionada = parseInt(prompt("Ingrese el ID de la prenda que desea comprar:"));
-const remeraBuscada = remeras.find(remera => remera.id === prendaSeleccionada);
-const buzoBuscado = buzos.find(buzo => buzo.id == prendaSeleccionada);
-const pantalonBuscado = pantalones.find(pantalon => pantalon.id == prendaSeleccionada);
+//Solicito al usuario el ID del producto
+let productoSeleccionado = parseInt(prompt("Ingrese el ID del producto que desea comprar:"));
+const tablaBuscada = tabla.find(tabla => tabla.id === productoSeleccionado);
+const tonkatsuBuscado = tonkatsu.find(tonkatsu => tonkatsu.id == productoSeleccionado);
+const gyozasBuscada = gyozas.find(gyozas => gyozas.id == pruductoSeleccionado);
 
-if (prendaSeleccionada <= 0) {
+if (productoSeleccionado <= 0) {
     alert("Ingresa un ID valido");
-} else if (prendaSeleccionada <=4) {
-    alert("Seleccionaste " +  remeraBuscada.nombre + "\nToca aceptar para agregarlo a tu carrito"); 
-} else if (prendaSeleccionada <=7) {
-    alert("Seleccionaste " + buzoBuscado.nombre + "\nToca aceptar para agregarlo a tu carrito");
-} else if (prendaSeleccionada <= 9) {
-    alert("Seleccionaste " + pantalonBuscado.nombre + "\nToca aceptar para agregarlo a tu carrito");
+} else if (productoSeleccionado <=4) {
+    alert("Seleccionaste " +  tablaBuscada.nombre + "\nToca aceptar para agregarlo a tu carrito"); 
+} else if (productoSeleccionado <=7) {
+    alert("Seleccionaste " + tonkatsuBuscado.nombre + "\nToca aceptar para agregarlo a tu carrito");
+} else if (productoSeleccionado <= 9) {
+    alert("Seleccionaste " + gyozasBuscada.nombre + "\nToca aceptar para agregarlo a tu carrito");
 } else {
     alert("Ingresa un ID valido");
 }
